d75e06649d39d67c772443da3c6fdae45e4e9776
Ian <ian.nancarrow@detroitlabs.com>
2017-03-23 14:38:30 -0400

    Added expected functions, started mainMenu & animation.

--- rpgls.js
+++ rpgls.js
@@ -37,13 +37,80 @@ var wipeScreen = function () {
 
 var rock;
 var paper;
-var sciccors;
+var scissors;
 var lizard;
 var spock;
 
 //*******Functions*******
 
+function mainMenu() {
+	wipeScreen();
+	menuAnimation();
+	userPrompt.question("", function(choice) {
+		if (choice == "1") {
 
+		} else if (choice == "2") {
+
+		} else if (choice == "3") {
+
+		} else if (choice == "4") {
+
+		} else if (choice == "5") {
+
+		} else if (choice == "0") {
+
+		} else {
+			console.log("INCORRECT ENTRY.")
+			sleep(2000);
+			sleep(1000);
+			mainMenu();
+		}
+	});
+}
+
+function menuAnimation() {
+	sleep(1000);
+	console.log("\n\n\n\n\n         ROCK                              ");
+	sleep(200);
+	wipeScreen();
+	sleep(200);
+	console.log("\n\n                  PAPER                    ");
+	sleep(200);
+	wipeScreen();
+	sleep(200);
+	console.log("\n\n\n\n\n                           SCISSORS        ");
+	sleep(200);
+	wipeScreen();
+	sleep(200);
+	console.log("\n\n\n\n\n\n\n\n                  LIZARD                   ");
+	sleep(200);
+	wipeScreen();
+	sleep(200);
+	console.log("\n\n\n\n\n                   SPOCK                   ");
+	sleep(200);
+	wipeScreen();
+	sleep(2000);
+	console.log("\n** Rock, Paper, Scissors, Lizard, Spock! **");
+	sleep(1000);
+	console.log("\n     - - - - Choose a gesture! - - - - \n")
+	console.log("               1) Rock\n               2) Paper\n               3) Scissors\n               4) Lizard\n               5) Spock\n\n               0) Quit")
+}
+
+function comparison() {
+
+}
+
+function win() {
+
+}
+
+function lose() {
+
+}
+
+function draw() {
+
+}
 
 
 
@@ -52,7 +119,7 @@ var spock;
 
 //*******Runners********
 
-
+mainMenu();
 
 
 
